0c903f68d762231b0db6ff99f71b545fb69fa8b8
andrewblgithub <andrewblichtenstein@gmail.com>
2017-12-06 17:36:40 -0500

    have components displaying, working on individualizing video data

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,17 +1,16 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+var VideoList = (props) => {
+  return (
+    <div className="video-list">
+      {props.videos.map((video) => {
+        return (<div><VideoListEntry video={video}/></div>);
+      })}
+    </div>);
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
-  videos: exampleVideoData
+  videos: React.PropTypes.array.isRequired
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
